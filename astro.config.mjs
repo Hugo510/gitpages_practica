5a35099deb1c0219769a79500bf66177a36be85c
Hugo Perez <hugoperez510@hotmail.com>
2025-06-11 14:46:23 -0600

    Configurar el despliegue en GitHub Pages y ajustar la configuración del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Configuración para GitHub Pages
+    site: 'https://hugo510.github.io',
+    base: '/gitpages_practica',
+});
